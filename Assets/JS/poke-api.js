9094b9c08354de16c64f77c2cee1221addbcceed
brenda <bbritob8@gmail.com>
2023-09-14 17:44:17 -0300

    ajuste

--- Assets/JS/poke-api.js
+++ Assets/JS/poke-api.js
@@ -1,5 +1,9 @@
 const pokeapi = {}
 
+pokeapi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
 pokeapi.getPokemons = (offset = 0, limit = 10) => {
 
     const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
@@ -7,5 +11,8 @@ pokeapi.getPokemons = (offset = 0, limit = 10) => {
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.log(error))
+        .then((pokemonList) => pokemonList.map(pokeapi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) =>  pokemonsDetails)  
+    
 }
